b5eb2f9a6add19b108c33dbec5cd4cde9687abd2
KaedeharaMilan <milanparade997@gmail.com>
2026-04-21 08:04:24 +0700

    some changes

--- lib/utils/api_endpoints.dart
+++ lib/utils/api_endpoints.dart
@@ -1,8 +1,8 @@
 class ApiEndpoints {
-  static const bool _useLocal = true;
+  static const bool _useLocal = false;
   static String baseUrl = _useLocal
       ? 'https://maverick-unaccessional-salutatorily.ngrok-free.dev/api'
-      : 'http://lsp-mkc-backend.test/api';
+      : 'http://ujikomp.lspmkc.or.id/api';
   static _AuthEndPoints authEndPoints = _AuthEndPoints();
 
   static Map<String, String> headers = {
@@ -24,4 +24,5 @@ class _AuthEndPoints {
   final String logoutPoint = '/logout';
   final String resendVerifyPoint = '/verify-email/resend';
   final String userPoint = '/user';
+  
 }
